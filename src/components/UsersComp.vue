7ac750af310d9131c3a98cd06eaaa788fa75d1b2
TomasBe24 <tomas.berias1@hotmail.com>
2022-06-23 17:26:31 -0300

    Trabajo finalizado

---
+++ src/components/UsersComp.vue
@@ -0,0 +1,70 @@
+<template>
+
+  <section class="src-components-users-comp">
+
+    <div class="jumbotron">
+    
+      <h1>Usuarios</h1>
+
+      <div v-if="usuarios.length" class="table-responsive">
+        <table class="table table-dark">
+          <tr>
+            <th>Nombre</th>
+            <th>Edad</th>
+            <th>Email</th>
+          </tr>
+          <tr v-for="(usuario, index) in usuarios" :key="index">
+            <td> {{ usuario.nombre }} </td>
+            <td> {{ usuario.edad }} </td>
+            <td> {{ usuario.email }} </td>
+          </tr>
+        </table>
+      </div>
+        <h5 v-else class="alert alert-danger">
+          No se encontraron usuarios
+        </h5>
+    
+    </div>
+
+  </section>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-components-users-comp',
+    props: [],
+    created () {
+      this.getUsuarios()
+
+    },
+    data () {
+      return {
+        url: 'https://628c22f5a3fd714fd02d3e5b.mockapi.io/usuarios',
+        usuarios: []
+      }
+    },
+    methods: {
+      async getUsuarios(){
+        try{
+          let { data } = await this.axios(this.url)
+          this.usuarios = data
+        }
+        catch(error){
+          console.error('Error Axios', error);
+        }
+      }
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+
+import {router} from './router'
+import './form'
+import './axios'
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
